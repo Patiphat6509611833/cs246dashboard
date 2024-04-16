cee527c0b8bb999c87adbb8932240ce024120e8d
Patiphat6509611833 <145008089+Patiphat6509611833@users.noreply.github.com>
2024-04-17 04:29:12 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,10 +53,11 @@ if option == "โรงอาหาร":
        
         st.header('')
         factors = df['ปัจจัยที่ท่านใช้ในการเลือกซื้ออาหารในโรงอาหารมหาวิทยาลัย'].str.split(',').explode().str.strip()
-        top_factors = df['ปัจจัยที่ท่านใช้ในการเลือกซื้ออาหารในโรงอาหารมหาวิทยาลัย'].value_counts().head(5)
+        top_factors = factors.value_counts().head(5)
         fig2 = px.bar(top_factors, x=top_factors.values, y=top_factors.index, orientation='h', labels={'x': 'จำนวน', 'y': 'ปัจจัย'})
-       
-    st.plotly_chart(fig2)
+
+        st.plotly_chart(fig2) 
+
 
     with col2:
         st.header('โรงอาหารที่นักศึกษามักใช้บริการ')
