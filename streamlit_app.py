973de4e726ed2aefc8485440ad1fbe495e070d26
Patiphat6509611833 <145008089+Patiphat6509611833@users.noreply.github.com>
2024-04-17 04:52:30 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,8 +92,8 @@ if option == "ร้านค้าภายนอก":
         fig5 = px.pie(df, names='โดยปกติแล้วท่านซื้อ/สั่งอาหารจากร้านค้าภายนอกหรือไม่')
         st.plotly_chart(fig5)
        
-        st.header('5 ปัจจัยที่ส่งผลมากที่สุดต่อการใช้บริการโรงอาหาร')
-        factors = df['ปัจจัยที่ท่านใช้ในการเลือกซื้ออาหารที่ร้านอาหารนอกมหาวิทยาลัย'].str.split(',').explode().str.strip()
+        st.header('5 ปัจจัยที่ส่งผลมากที่สุดต่อการใช้บริการนอกมหาวิทยาลัย')
+        factors = df['ปัจจัยที่ท่านใช้ในการเลือกซื้ออาหารที่นอกมหาวิทยาลัย'].str.split(',').explode().str.strip()
         top_factors = factors.value_counts().head(5)
         fig6 = px.bar(top_factors, x=top_factors.values, y=top_factors.index, orientation='h', labels={ 'x': '','y': 'ปัจจัย'})
 
@@ -108,6 +108,23 @@ if option == "ร้านค้าภายนอก":
 
 # เรียกใช้ Streamlit เพื่อแสดงผล
         st.plotly_chart(fig_pie)
+        st.header('ช่วงเวลาที่นักศึกษามักใช้บริการร้านอาหารนอกมหาวิทยาลัย')
+        eating_times = df['ท่านซื้อ/สั่งอาหารในช่วงเวลาใดบ้าง'].str.split(',').explode().str.strip()
+
+
+        eating_times_count = eating_times.value_counts()
+
+
+        eating_times_df = pd.DataFrame({'time': eating_times_count.index, 'count': eating_times_count.values})
+
+
+        eating_times_df = eating_times_df.sort_values(by='time')
+
+
+        fig_line2 = px.line(eating_times_df, x='time', y='count',  markers=True)
+
+
+        st.plotly_chart(fig_line2)
 
 
 
