d56c90d3e3543fd15b86cf629ec6b5a1e7f03f13
Patiphat6509611833 <145008089+Patiphat6509611833@users.noreply.github.com>
2024-04-17 04:21:41 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,12 +32,6 @@ left_column, middle_column, right_column = st.columns(3)
 with left_column:
     st.subheader("จำนวนผู้ตอบแบบสอบถาม:")
     st.subheader("101 คน")
-with middle_column:
-    st.subheader("Average Rating:")
-    st.subheader("")
-with right_column:
-    st.subheader("Average Sales Per Transaction:")
-    st.subheader("  ")
 
 st.markdown("""---""")
 
@@ -53,23 +47,15 @@ if option == "โรงอาหาร":
     col1, col2 = st.columns(2)
     with col1:
 
-        st.header('จำนวนนักศึกษาที่ใช้บริการโรงอาหาร')
+        st.header('')
         fig = px.pie(df, names='โดยปกติแล้วท่านซื้ออาหารจากโรงอาหารหรือไม่')
         st.plotly_chart(fig)
 
-
-    
-
-
-
-
-
-
-
-
-
-
-fig = px.pie(df, names='ท่านพักอยู่ที่ใด')
+    with col2:
+        st.header('')
+        mcanteen = df['โรงอาหารที่ท่านใช้บริการเป็นประจำ'].str.split(',').explode().str.strip()
+        fig4 = px.bar(mcanteen.value_counts(), x=mcanteen.value_counts().index, y=mcanteen.value_counts().values, labels={'y': 'จำนวน'})
+        st.plotly_chart(fig4)
 
 
 
@@ -78,23 +64,18 @@ fig = px.pie(df, names='ท่านพักอยู่ที่ใด')
 
 
 
-mcanteen = df['โรงอาหารที่ท่านใช้บริการเป็นประจำ'].str.split(',').explode().str.strip()
-fig4 = px.bar(mcanteen.value_counts(), x=mcanteen.value_counts().index, y=mcanteen.value_counts().values, labels={'y': 'จำนวน'})
 
 
 
 
 
-# First column: Pie chart
-col1, col2 = st.columns(2)  # แบ่งหน้าจอเป็น 2 คอลัมน์
 
-with col1:
-    st.header('ที่พักของนักศึกษา')
-    st.plotly_chart(fig)  # แสดง Pie chart ในคอลัมน์แรก
 
-# Second column: Bar chart
-with col2:
-    
-    st.header('โรงอาหารที่ได้รับความนิยม')
-    st.plotly_chart(fig4)  # แสดง Bar chart ของโรงอาหารในคอลัมน์ที่สาม
-    
+
+
+
+
+
+
+
+
